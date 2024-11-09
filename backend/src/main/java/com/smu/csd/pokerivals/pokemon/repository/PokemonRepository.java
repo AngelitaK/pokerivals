37b7950cdb389c526da2f9293242bc467135ef72
Arcticsun1 <160473700+Arcticsun1@users.noreply.github.com>
2024-11-09 23:00:45 +0800

    added eager fetching for testing

--- backend/src/main/java/com/smu/csd/pokerivals/pokemon/repository/PokemonRepository.java
+++ backend/src/main/java/com/smu/csd/pokerivals/pokemon/repository/PokemonRepository.java
@@ -1,10 +1,18 @@
 package com.smu.csd.pokerivals.pokemon.repository;
 
 import com.smu.csd.pokerivals.pokemon.entity.Pokemon;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
     Optional<Pokemon> findByName(String name);
+
+    @NotNull
+    @EntityGraph(attributePaths = { "abilities", "moves" })
+    Optional<Pokemon> findById(@NotNull Integer id);
 }
