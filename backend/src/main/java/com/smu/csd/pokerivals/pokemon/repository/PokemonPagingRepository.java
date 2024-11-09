3c1d6e87618dc940b9d413ef8b70d0b3c0815091
Arcticsun1 <160473700+Arcticsun1@users.noreply.github.com>
2024-11-09 21:42:31 +0800

    Revert "pokemon entity"
    This reverts commit 59175af818b20482a4c07f5783652a650d845e9c.

--- backend/src/main/java/com/smu/csd/pokerivals/pokemon/repository/PokemonPagingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smu.csd.pokerivals.pokemon.repository;
-
-import com.smu.csd.pokerivals.pokemon.entity.Pokemon;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PokemonPagingRepository extends PagingAndSortingRepository<Pokemon,Integer> {
-
-}
